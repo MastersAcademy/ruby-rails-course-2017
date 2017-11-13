38d6935c94761a44eebf1d049d169f6a6377c1d0
kostynskyi <kostynsky@gmail.com>
2017-11-13 18:25:34 +0200

    One more fix

--- homeworks/vladislav.kostynskyi/main.rb
+++ homeworks/vladislav.kostynskyi/main.rb
@@ -9,31 +9,31 @@ class Waiter < Employee
   end
   
   def waiter_comming
-    puts "Customer is comming to the restourant"
+    puts 'Customer is comming to the restourant'
   end
   
   def self_presentation
-    puts "Waiter: Hello, My name is #{@first_name}. Are you ready to take an order?"
+    puts 'Waiter: Hello, My name is #{first_name}. Are you ready to take an order?'
   end
 
   def take_order 
-    puts "Waiter: Please enter the name of the dish you want to order"
+    puts 'Waiter: Please enter the name of the dish you want to order'
   end
   
   def claim_info
-    puts "Waiter: Hey, Your food will be ready in 20 minutes"
+    puts 'Waiter: Hey, Your food will be ready in 20 minutes'
   end
   
   def deliver_order
-    puts "Waiter: Here is your order"
+    puts 'Waiter: Here is your order'
   end
   
   def deliver_bill
-    puts "Waiter: Here is your bill"
+    puts 'Waiter: Here is your bill'
   end
   
   def thanks
-    puts "Waiter: Thanks for comming and tips!"
+    puts 'Waiter: Thanks for comming and tips!'
   end
   
 end
@@ -45,11 +45,11 @@ end
   end
    
   def start_coocking
-    puts "Cook: #{waiter_name}, say to the client, his food will be ready in 20 minutes"
+    puts 'Cook: #{@waiter_name}, say to the client, his food will be ready in 20 minutes'
   end
 
   def food_is_ready
-    puts "Cook: #{waiter_name}, Take a food and deliver to the customer"
+    puts 'Cook: #{@waiter_name}, Take a food and deliver to the customer'
   end
 end
 
@@ -60,30 +60,30 @@ class Customer
   end
   
   def customer_comming
-    puts "Waiter is comming to the customer"
+    puts 'Waiter is comming to the customer'
   end
 
   def new_order
     @order = gets.chomp
-    puts "Yes, I want to order: #{@order}"
+    puts 'Yes, I want to order: #{@order}'
   end
   
   def waiting_for_food
-    puts "Ok, we are waiting"
+    puts 'Ok, we are waiting'
   end
     
   def ask_for_bill
-    puts "Customer: #{@waiter}, Please give my my bill"
+    puts 'Customer: #{@waiter}, Please give my my bill'
   end
   
   def thanks_you
-    puts "Customer: Thanks for the tasty food! I would like to tip you"
+    puts 'Customer: Thanks for the tasty food! I would like to tip you'
   end
   
   def tips
-    puts "Customer: How much do you want?"
-    @money = gets.chomp
-    puts "Customer: Here are your #{@money} dollars!"
+    puts 'Customer: How much do you want?'
+    money = gets.chomp
+    puts 'Customer: Here are your #{money} dollars!'
   end
 end  
 
