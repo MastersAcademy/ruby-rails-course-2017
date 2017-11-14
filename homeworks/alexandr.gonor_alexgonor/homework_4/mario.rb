5ab2e0bf1f2e00cf50b3911b8475a6ed7f76ea3e
alexgonor <gonor.alexandr@gmailcom>
2017-11-14 19:58:54 +0200

    do some validation for mario

--- homeworks/alexandr.gonor_alexgonor/homework_4/mario.rb
+++ homeworks/alexandr.gonor_alexgonor/homework_4/mario.rb
@@ -1,9 +1,18 @@
 def mario(steps)
-  kostil = steps+1      # I fill bad about this:(
-  (kostil).times do |i|
-    p ' '*(steps-i) + '#'*i + '  ' + '#'*i
+  kostil = steps + 1 # I fill bad about this:(
+  kostil.times do |i|
+    p ' ' * (steps - i) + '#' * i + '  ' + '#' * i
   end
 end
-puts "Give me number of steps"
-number = gets
-mario(number.to_i)
+
+def welcome
+  puts 'Give me number of steps'
+  number = gets.chomp
+  if (1..20).cover?(number.to_i)
+    mario(number.to_i)
+  else
+    puts "It's too high for Mario..."
+    return welcome
+  end
+end
+puts welcome
