927e430ab3a73c4b05272c6c7f4b2cdd5ed8d811
Victor Bert <orion7@ukr.net>
2017-11-18 16:20:14 +0200

    homework4

--- homeworks/victor.bert_victor-bert/homework_4/pyramids.rb
+++ homeworks/victor.bert_victor-bert/homework_4/pyramids.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+class Pyramid
+  def prompt
+    print 'Set the pyramid height (min value 2): '
+    height = gets.chomp.to_i
+    if height >= 2 && height < 77
+      self.build(height)
+    else
+      puts 'Invalid parameter, try it again.'
+      self.prompt
+    end
+  end
+
+  def build (height = 4 )
+    height.times do |index|
+      rh = (0...height).map {|i| index >= i ? '#' : ' ' }
+      puts [rh.reverse.join(''), rh.join('')].join('  ')
+    end
+  end
+end
+
+pyramid = Pyramid.new
+pyramid.prompt
