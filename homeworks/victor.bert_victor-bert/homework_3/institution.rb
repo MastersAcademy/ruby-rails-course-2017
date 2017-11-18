94e11c69307ed8dabd11592ce8211b105b092332
Victor Bert <orion7@ukr.net>
2017-11-18 16:07:26 +0200

    fixed hw3

--- homeworks/victor.bert_victor-bert/homework_3/institution.rb
+++ homeworks/victor.bert_victor-bert/homework_3/institution.rb
@@ -14,7 +14,9 @@ class Institution
 		@time = Time.new
 	end
 
-	def opened; @time.hour <= 23; end
+	def opened
+		@time.hour <= 23
+	end
 end
 
 class Employer
@@ -27,37 +29,78 @@ class Employer
 end
 
 class Waitress < Employer
-	attr_accessor :name
 
 	def initialize (name = ['Ann', 'Ramona', 'Kate', 'Diana' ][rand(0..3)] )
 		@name = name
 	end
 
-	def menu;  puts "Waitress #{@name} bring menu to visitor"; true; end
-	def order; puts 'Waitress takes order';                    true; end
-	def give;  puts 'Waitress gives dishes';                   true; end
-	def bill;  puts 'Waitress brings bill';                    true; end
-	def clean; puts 'Waitress cleans table';                   true; end
+	def menu
+		puts "Waitress #{name} bring menu to visitor"
+		self
+	end
+	def order
+		puts 'Waitress takes order'
+		self
+	end
+	def give
+		puts 'Waitress gives dishes'
+		self
+	end
+	def bill
+		puts 'Waitress brings bill'
+		self
+	end
+	def clean
+		puts 'Waitress cleans table'
+		self
+	end
 end
 
 class Visitor
 	attr_accessor :name, :table
 
-	def initialize(name, table )
+	def initialize (name, table )
 		@name = name
 		@table = table
 	end
 
-	def goHome; puts 'Visitor returns to home!!!'; true; end
+	def go_home
+		puts 'Visitor returns to home!!!'
+		self
+	end
 
-	def choose;    puts "Visitor #{@name.upcase} choose a table ##{table} and sit"; true; end
-	def call;      puts "Visitor call a waitress";                                  true; end
-	def ask(type); puts "Visitor ask a #{type}";                                    true; end
-	def make;      puts "Visitor make the order";                                   true; end
-	def awaits;    puts "Visitor awaits";                                           true; end
-	def eats;      puts "Visitor eats these dishes";                                true; end
-	def pay;       puts "Visitor paid the bill";                                    true; end
-	def leave;     puts "Visitor leave the institution";                            true; end
+	def choose
+		puts "Visitor #{@name.upcase} choose a table ##{table} and sit"
+		self
+	end
+	def call
+		puts "Visitor call a waitress"
+		self
+	end
+	def ask(type)
+		puts "Visitor ask a #{type}"
+		self
+	end
+	def make
+		puts "Visitor make the order"
+		self
+	end
+	def awaits
+		puts "Visitor awaits"
+		self
+	end
+	def eats
+		puts "Visitor eats these dishes"
+		self
+	end
+	def pay
+		puts "Visitor paid the bill"
+		self
+	end
+	def leave
+		puts "Visitor leave the institution"
+		self
+	end
 end
 
 
@@ -71,7 +114,9 @@ visitor = Visitor.new('beginnerCoder', rand(1..10) )
 
 # puts flow
 if institution.opened
-	visitor.choose && visitor.call && visitor.ask('menu') && visitor.make
+	visitor.choose && visitor.call && visitor.ask('menu')
+	waitress.menu
+	visitor.make
 	waitress.order
 	visitor.awaits
 	waitress.give
@@ -80,5 +125,5 @@ if institution.opened
 	visitor.pay && visitor.leave
 	waitress.clean
 else
-	visitor.goHome
+	visitor.go_home
 end
