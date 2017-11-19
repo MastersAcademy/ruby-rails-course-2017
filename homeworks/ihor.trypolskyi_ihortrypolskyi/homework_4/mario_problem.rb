f0d0493c38775afd4173efbe188c0eba66af323d
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-11-19 22:02:53 +0200

    refactored

--- homeworks/ihor.trypolskyi_ihortrypolskyi/homework_4/mario_problem.rb
+++ homeworks/ihor.trypolskyi_ihortrypolskyi/homework_4/mario_problem.rb
@@ -1,6 +1,6 @@
 def piramida(height = 1, char = "#")
   if height < 2
-    return puts "height shold be >=2"
+    return puts "integer should be >=2"
   else
     height.times do |time|
       a = (" ," * (height - (time + 1))).split(",") + (char.split) * (time + 1)
