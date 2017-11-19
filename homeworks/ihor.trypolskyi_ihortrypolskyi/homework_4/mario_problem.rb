51fca8a9a0bab032d8aba5c9edd1abcb23cc8965
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-11-19 21:49:05 +0200

    refactored

--- homeworks/ihor.trypolskyi_ihortrypolskyi/homework_4/mario_problem.rb
+++ homeworks/ihor.trypolskyi_ihortrypolskyi/homework_4/mario_problem.rb
@@ -1,15 +1,15 @@
-def piramida(height = 1, char="#")
+def piramida(height = 1, char = "#")
   if height < 2
     return puts "height shold be >=2"
   else
     height.times do |time|
-      a = (" ," * (height - (time + 1))).split(",") << (char.split) * (time + 1)
+      a = (" ," * (height - (time + 1))).split(",") + (char.split) * (time + 1)
       puts a.join() + "  " + a.reverse.join()
     end
   end
 end
-puts "enter any ineger"
-c=gets.chomp.to_i
+puts "enter any integer"
+height = gets.chomp.to_i
 puts "enter any character"
-f=gets.chomp.to_s
-piramida(c, f)
+char = gets.chomp.to_s
+piramida(height, char)
