82ba6e86c745c40af3f9cfae8a453953b8bcdfc3
Constantin Rusenko <krusenko@gmail.com>
2017-11-26 17:26:55 +0200

    Fix#2

--- homeworks/constantin.rusenko_ConstantinRusenko/homework_5/Metaprogramming.rb
+++ homeworks/constantin.rusenko_ConstantinRusenko/homework_5/Metaprogramming.rb
@@ -1,12 +1,12 @@
-Class MyMetaprogramming
+class MyMetaprogramming
   def initialize(hash)
     hash.each do |key, value|
-    	define_singleton_method(key) { puts "#{value}" }
-      end
+      define_singleton_method(key) { puts value.to_s }
+    end
   end
 end
 
-hash = {hello: 'Hello my friend)', play: 'Do you wanna play with me?', win:'Winner, winner, chicken dinner'}
+hash = {hello: 'Hello my friend)', play: 'Do you wanna play with me?', win: 'Winner, winner, chicken dinner'}
 newinstance = MyMetaprogramming.new(hash)
 puts newinstance.hello
 puts newinstance.play
