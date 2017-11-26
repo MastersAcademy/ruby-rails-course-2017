4ea14f9b38dd472f79a1961ac1ee5b41db07861c
Den Zubritskiy <katatsu12@gmail.com>
2017-11-26 23:22:45 +0200

    homework_5 and corrected homework_4

---
+++ homeworks/denis.zubritskiy_katatsu12/homework_5/homework_5.rb
@@ -0,0 +1,16 @@
+class Worker
+  attr_accessor :name, :surname, :salary, :specialty
+
+  def initialize(hash)
+    hash.each do |key, value|
+      define_singleton_method(key) { puts "#{value}" }
+    end
+  end
+end
+
+wor = Worker.new({ name: "Jon", surname: "X", salary: 50000, specialty: "chef" })
+
+puts wor.name
+puts wor.surname
+puts wor.salary
+puts wor.specialty
