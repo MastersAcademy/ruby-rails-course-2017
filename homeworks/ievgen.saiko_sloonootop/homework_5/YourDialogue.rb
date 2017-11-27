039cf29df788fee68a1d71d74176f0ceb9b35ca5
SLooNooPooTAM <C:\Users\\AppData\Roaming\The Bat!>
2017-11-27 18:04:47 +0200

    homework_5 fixed

--- homeworks/ievgen.saiko_sloonootop/homework_5/YourDialogue.rb
+++ homeworks/ievgen.saiko_sloonootop/homework_5/YourDialogue.rb
@@ -1,13 +1,12 @@
 class YourDialogue
   def initialize (questions)
-    @questions = questions
-    @answers = Hash.new{}
+    @answers = {}
     puts "Let`s begin!"
     
     # Hash to the methods
-    @questions.each do |key, value| 
-      puts value
-      define_singleton_method(key) { puts value }
+    questions.each do |key, value| 
+      print value
+      define_singleton_method(key) { print value }
       
       # Hash of the answers (input)
       @answers[key] = String(gets.chomp)
@@ -21,20 +20,20 @@ end
 
 # Hash of the questions (input)
 questions = { 
-  hello: "Welcome to the World Championship of the codeball!", 
-  play: "Do you wanna play the codeball?", 
-  name: "Enter your name and become our coach!",  
-  congr: "Congr., you are a coach of the Jamaica codeball team!", 
-  game: "What kind of the game do you prefer? (atack or defence)", 
-  start: "Type Yes to start or No to exit" 
+  hello: "Welcome to World Championship of codeball! ", 
+  play: "Do you wanna play codeball? ", 
+  name: "Enter your name and become our coach! ",  
+  congr: "Congr., you are a coach of the Jamaica codeball team! ", 
+  game: "What kind of the game do you prefer? (atack or defence) ", 
+  start: "Type Yes to start or No to exit " 
 }
 
 # Dialogue
 puts "Do you ready?"
-print 'Press 1 if your are ready or 2 - if not: '
+print "Press 1 if your are ready or 2 - if not: "
 answer = gets.chomp.to_i
  case
  when answer == 1
    dialogue = YourDialogue.new(questions)
-   puts "Thank you! Try the new methods in the terminal"
+   puts "Thank you! Try new methods in the terminal"
  end
