e8b398e295f003df069ee6320e7d358ce08fd5a3
alexgonor <gonor.alexandr@gmail.com>
2017-11-28 15:06:24 +0200

    Update hash_to_json_and_back.rb

--- homeworks/alexandr.gonor_alexgonor/homework_6/hash_to_json_and_back.rb
+++ homeworks/alexandr.gonor_alexgonor/homework_6/hash_to_json_and_back.rb
@@ -1,15 +1,28 @@
 require 'json'
 
-user_hash = {'user' =>
+class User
+
+  def initialize(params)
+    params.each do |key, value|
+      define_singleton_method(key) {  puts(value); value }
+    end
+  end
+end
+
+hash = { first_name: 'Alex', last_name: 'Gonor' }
+params = hash.merge({:full_name => hash.values.join(" ")})
+user = User.new(params)
+
+puts user_hash = {'user' =>
   {
   'first_name_and_last_name' => [
-  'first_name' => "Alex",
-  'last_name' => "Gonor"
+  'first_name' => "#{user.first_name}",
+  'last_name' => "#{user.last_name}"
   ],
-  'full_name' => "Alex.Gonor"
+  'full_name' => "#{user.full_name}"
   }
   }
-
+  
 puts json = JSON.pretty_generate(user_hash)
 puts "===================================="
 puts parsed = JSON.parse(json)
