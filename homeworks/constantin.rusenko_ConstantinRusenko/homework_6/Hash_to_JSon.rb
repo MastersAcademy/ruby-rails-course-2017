efcf4e0d8c1d4f38a305cace4be498b7d689cc2f
Constantin Rusenko <krusenko@gmail.com>
2017-12-03 16:36:02 +0200

    Homework#6

---
+++ homeworks/constantin.rusenko_ConstantinRusenko/homework_6/Hash_to_JSon.rb
@@ -0,0 +1,6 @@
+require 'json'
+hash = {hello: 'Hello my friend)', play: 'Do you wanna play with me?', win: 'Winner, winner, chicken dinner'}
+puts hash
+puts variable= hash.to_json
+my_hash = JSON.parse(variable)
+puts my_hash
