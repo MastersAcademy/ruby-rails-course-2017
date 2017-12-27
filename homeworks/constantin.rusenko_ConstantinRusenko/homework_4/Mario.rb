4cdcb309d9debb1c8ca0e15c9466bb134648e7f0
Constantin Rusenko <krusenko@gmail.com>
2017-12-27 17:57:20 +0200

    Rubocop_hm#4

--- homeworks/constantin.rusenko_ConstantinRusenko/homework_4/Mario.rb
+++ homeworks/constantin.rusenko_ConstantinRusenko/homework_4/Mario.rb
@@ -1,16 +1,15 @@
 class Homework4
-	def Mario
-	 	puts "Input number (number >=2)"
-		number = gets.to_i
-		if number < 2
-			puts "Error"
-		else		
-			i = 1
-			i.upto(number) do |i|
-  				puts ' '*(number-i) + '#'*i + '  ' + '#'*i
-  			end
-  		end
-  	end		
+  def Mario
+    puts "Input number (number >=2)"
+    number = gets.to_i
+    if number < 2
+      puts "Error"
+    else		
+      1.upto(number) do |i|
+        puts ' '*(number-i) + '#'*i + '  ' + '#'*i
+      end
+    end
+  end		
 end
 
 hmwrk = Homework4.new
