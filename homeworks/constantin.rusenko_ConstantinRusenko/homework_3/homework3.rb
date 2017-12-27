920657907108be437ab2e06211c3594a7245a9be
Constantin Rusenko <krusenko@gmail.com>
2017-12-27 17:52:49 +0200

    Rubocop_hm#3

--- homeworks/constantin.rusenko_ConstantinRusenko/homework_3/homework3.rb
+++ homeworks/constantin.rusenko_ConstantinRusenko/homework_3/homework3.rb
@@ -1,118 +1,102 @@
-class Employer
-  
- attr_accessor :name, :salary
+class Employer 
+  attr_accessor :name, :salary
 
-     def initialize(name, salary)
-         @name = name
-         @salary = salary         
-     end
+  def initialize(name, salary)
+    @name = name
+    @salary = salary         
+  end
  
-    def salary_day
-        puts "#{@name} get #{@salary}"
-    end
+  def salary_day
+    puts "#{@name} get #{@salary}"
+  end
 end
 
-class Visitor
-  
- attr_accessor :name, :table_number
+class Visitor 
+  attr_accessor :name, :table_number
 
-     def initialize(name)
-         @name = name
-     end
+  def initialize(name)
+    @name = name
+  end
  
-     def choose_table(table)
-         puts "Visitor #{@name} choose table #{table.number}"
-     end
+  def choose_table(table)
+    puts "Visitor #{@name} choose table #{table.number}"
+  end
      
-    def call_waiter(waiter)
-        puts "Visitor #{@name} call waiter #{waiter.name}"
-    end
+  def call_waiter(waiter)
+    puts "Visitor #{@name} call waiter #{waiter.name}"
+  end
     
-    def call_maneger(maneger)
-        puts "Visitor #{@name} call manager #{maneger.name}"
-    end
+  def call_maneger(maneger)
+    puts "Visitor #{@name} call manager #{maneger.name}"
+  end
     
-     def make_order
-         puts "Visitor #{@name} make his order "
-     end
+  def make_order
+    puts "Visitor #{@name} make his order "
+  end
      
-     def eat_dish(order)
-        puts "#{order.ordered_dish} is missing"
-     end
+  def eat_dish(order)
+    puts "#{order.ordered_dish} is missing"
+  end
      
-    def ask_bill(waiter)
-        puts "Visitor #{@name} call waiter #{waiter.name} to bring his bill "
-    end
+  def ask_bill(waiter)
+    puts "Visitor #{@name} call waiter #{waiter.name} to bring his bill "
+  end
     
-     def pay_bill(bill)
-         puts "Visitor #{@name} pay amount #{bill.amount}"
-     end
-     
+  def pay_bill(bill)
+    puts "Visitor #{@name} pay amount #{bill.amount}"
+  end     
 end
-
 
 class Waiter < Employer
   
-
-     def accept_order
-         puts "Waiter #{@name} take order"
-     end
+  def accept_order
+    puts "Waiter #{@name} take order"
+  end
      
-     def bring_dish(order)
-         puts "Waiter #{@name} bring #{order.ordered_dish}"
-     end
+  def bring_dish(order)
+    puts "Waiter #{@name} bring #{order.ordered_dish}"
+  end
      
-     def bring_bill
-         puts "Waiter #{@name} bring bill"
-     end
-     
+  def bring_bill
+    puts "Waiter #{@name} bring bill"
+  end  
 end
-
 
 class Maneger < Employer
   
-    def talk_with_visitors(visitor)
-        puts "Maneger #{@name} talk wiht Visitor #{visitor.name}"
-    end
-     
+  def talk_with_visitors(visitor)
+    puts "Maneger #{@name} talk wiht Visitor #{visitor.name}"
+  end   
 end
-
 
 class Cook < Employer
 
-     def cooking(order)
-          puts "Cook #{@name} make #{order.ordered_dish}"
-     end
-   
+  def cooking(order)
+    puts "Cook #{@name} make #{order.ordered_dish}"
+  end 
+
 end
-
-
          
 class Table
-
-    attr_accessor :number
+  attr_accessor :number
  
-    def occupy(visitor)
-        puts "Visitor #{visitor.name} occupy table number #{number}"
-     end
+  def occupy(visitor)
+    puts "Visitor #{visitor.name} occupy table number #{number}"
+  end
 
-     def leave(visitor)
-        puts "Visitor #{visitor.name} leave table number #{number}"
-     end
+  def leave(visitor)
+    puts "Visitor #{visitor.name} leave table number #{number}"
+  end
 
 end
 
-
 class Order
-  
-    attr_accessor :ordered_dish, :price
+  attr_accessor :ordered_dish, :price
     
-    def initialize(ordered_dish, price)
-         @ordered_dish = ordered_dish
-         @price = price         
-     end
-    
-    
+  def initialize(ordered_dish, price)
+    @ordered_dish = ordered_dish
+    @price = price         
+  end
 
 end
 
@@ -120,29 +104,28 @@ end
 
     
 class Bill
+  attr_accessor :amount
     
-    attr_accessor :amount
-    
-    def am(order)
-        @amount = order.price 
-    end
+  def am(order)
+    @amount = order.price 
+  end
     
 end    
 
 
 ###################################
 
-visitor = Visitor.new("Skrypnikov Vladislav")
+visitor = Visitor.new('Skrypnikov Vladislav')
 table = Table.new
 table.number = 2
 visitor.choose_table(table)
 table.occupy(visitor)
-waiter = Waiter.new("Yana", 3500)
+waiter = Waiter.new('Yana', 3500)
 visitor.call_waiter(waiter)
 visitor.make_order
-order = Order.new("Fish", 50)
+order = Order.new('Fish', 50)
 waiter.accept_order
-cook = Cook.new("Elena", 5000)
+cook = Cook.new('Elena', 5000)
 cook.cooking(order)
 waiter.bring_dish(order)
 visitor.eat_dish(order)
@@ -151,7 +134,7 @@ waiter.bring_bill
 bill = Bill.new
 bill.am(order)
 visitor.pay_bill(bill)
-maneger = Maneger.new("Leonid", 8000)
+maneger = Maneger.new('Leonid', 8000)
 visitor.call_maneger(maneger)
 maneger.talk_with_visitors(visitor)
 table.leave(visitor)
