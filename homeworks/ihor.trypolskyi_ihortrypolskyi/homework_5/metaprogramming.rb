9bf4cb2186d33a9c141c6ebe79de1e7794d76031
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2017-12-28 13:07:36 +0200

    rubocop check

--- homeworks/ihor.trypolskyi_ihortrypolskyi/homework_5/metaprogramming.rb
+++ homeworks/ihor.trypolskyi_ihortrypolskyi/homework_5/metaprogramming.rb
@@ -1,24 +1,24 @@
-#with initialize
+# with initialize
 class A
   def initialize(hash)
     hash.each do |key, value|
-      define_singleton_method (key) {puts "#{value}"}
+      define_singleton_method(key) { puts value.to_s }
     end
   end
 end
 
-hash = {hello: "Hello, babe", play: "Do you want play with me?"}
+hash = { hello: 'Hello, babe', play: 'Do you want play with me?' }
 
 a = A.new(hash)
 a.hello
 a.play
 
-#without initialize
+# without initialize
 class B
-  hash = {bye: "Bye, babe", no_play: "Sorry,I don't want to play with you"}
+  hash = { bye: 'Bye, babe', no_play: "Sorry,I don't want to play with you" }
 
   hash.each do |key, value|
-    define_method(key) {puts "#{value}"}
+    define_method(key) { puts value.to_s }
   end
 end
 
@@ -26,13 +26,18 @@ b = B.new
 b.bye
 b.no_play
 
+# without initialize
 class C
   def method_missing(method_name, args)
-    args[method_name]
+    method_name.to_s ? args[method_name] : super
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.args[method_name] || super
   end
 end
 
-hash = {yes: "fine", no: "bad"}
+hash = { yes: 'fine', no: 'bad' }
 c = C.new
 p c.yes(hash)
 p c.no(hash)
--- homeworks/ihor.trypolskyi_ihortrypolskyi/homework_7/exceptions.rb
+++ homeworks/ihor.trypolskyi_ihortrypolskyi/homework_7/exceptions.rb
@@ -4,76 +4,83 @@ require 'pp'
 class CoursesPresentError < StandardError
 end
 
+# customer has surname and has dinner
 class Customer
-  attr_reader :name
+  attr_reader :surname, :first_and_second_course, :dessert
   attr_accessor :dinner
 
-  def name=(name)
-    if name.nil? || name.size.zero?
-      raise ArgumentError, "Customer name can't be nil or empty"
+  def surname=(surname)
+    if surname.nil? || surname.size.zero?
+      raise ArgumentError, "Customer surname can't be nil or empty"
     end
-    @name = name
+    @surname = surname
   end
 
-  def initialize(name, dinner)
-    self.name = name.capitalize
+  def initialize(surname, dinner)
+    self.surname = surname.capitalize
     @dinner = dinner
+    @first_and_second_course = dinner.values.first(2).join(', ')
+    @dessert = dinner.values.last
   end
 
-  def has_dinner
+  def make_order
     dinner.each_value do |v|
-      if v.nil? || v.size.zero?
-        raise CoursesPresentError, "Dinner courses can't be nil or empty"
-      end
+      next unless v.nil? || v.size.zero?
+      raise CoursesPresentError, "Dinner courses can't be nil or empty"
     end
-    puts "Customer asked: I would have"
-    @dinner.each {|k, v| puts "#{v} for #{k} "}
+    puts 'Customer says: I would have'
+    @dinner.each { |k, v| puts "#{v} for #{k} " }
   end
 end
 
+# waiter checks order courses, take desert from menu.json, tells clarified_order
 class Waiter
   def double_check(customer)
     customer.dinner.each do |k, v|
-      if v.nil? || v.size.zero?
-        puts "Excuse me, Mr. #{customer.name}, what exactly would you like for #{k}?"
-        answer = []
-
-        File.open("menu.json", "r") do |file|
-            data = JSON.parse(file.read)
-            answer = data['desserts'].sample
-        end
-
-        if answer.nil? || answer.size.zero?
-          raise CoursesPresentError, "There's nil or empty array element in Menu.json file"
-        else
-          v << answer
-        end
-      end
+      next unless v.nil? || v.size.zero?
+      puts "Waiter: Mr. #{customer.surname}, what would you like for #{k}?"
+      parse_file
+      return v << @answer unless @answer.nil? || @answer.size.zero?
+      raise CoursesPresentError, 'Nil or empty element in Menu.json file'
     end
-    first_and_second_course = customer.dinner.values.first(2).join(", ")
-    dessert = customer.dinner.values.last
-    puts "Waiter: \"So, Mr. #{customer.name}, your order is: #{first_and_second_course} and #{dessert}\""
+  end
+
+  def parse_file
+    @answer = []
+    File.open('menu.json', 'r') do |file|
+      data = JSON.parse(file.read)
+      @answer = data['desserts'].sample
+    end
+  end
+
+  def clarified_order(customer)
+    puts "Waiter: 'So, Mr. #{customer.surname}, your order \
+is: #{customer.first_and_second_course} and #{customer.dessert}'"
   end
 end
 
-customer = Customer.new("john", first_course: "soup", second_course: "lasagna", dessert: "" )
+customer = Customer.new(
+  'Jackson',
+  first_course: 'soup',
+  second_course: 'lasagna',
+  dessert: ''
+)
 waiter = Waiter.new
 
-
 begin
-  customer.has_dinner
+  customer.make_order
 rescue CoursesPresentError => e
   puts "Error: #{e.message}"
 ensure
   waiter.double_check(customer)
+  waiter.clarified_order(customer)
 end
 
 begin
-  customer.name = nil
-rescue  ArgumentError => e
+  customer.surname = nil
+rescue ArgumentError => e
   puts "Error: #{e.message}"
 ensure
-  customer.name = 'ben'
+  customer.surname = 'slater'
   pp customer
 end
-
