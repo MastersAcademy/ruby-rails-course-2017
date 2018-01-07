db77407ccce25eb72d8123e8cbaba90717dc72b2
ihortrypolskyi <ihor.trypolskyi@gmail.com>
2018-01-07 19:55:34 +0200

    refactored

--- homeworks/ihor.trypolskyi_ihortrypolskyi/homework_10/tamagotchi.rb
+++ homeworks/ihor.trypolskyi_ihortrypolskyi/homework_10/tamagotchi.rb
@@ -1,5 +1,4 @@
-#Tamagochi via rack
-
+# Tamagochi via rack
 class Tamagotchi
   def initialize(name)
     @name = name
@@ -8,101 +7,126 @@ class Tamagotchi
   end
 
   def call(env)
-    @treat = "<div
-      <b><a href = \"/eat\">Feed</a> |
-      <a href = \"/play\">Play</a> |
-      <a href = \"/sleep\">Put to bed</a>
-      </div>"
+    @treat = '<a href = \'/eat\'>Feed</a> |
+              <a href = \'/play\'>Play</a> |
+              <a href = \'/sleep\'>Put to bed</a>'
 
-    @new_game = "<div
-      <b><a href = \"/new_game\">Start new game</a>
-      </div>"
+    @new_game = '<a href = \'/new_game\'>Start new game</a>'
 
     request = Rack::Request.new(env)
 
     case request.path_info
     when '/'
-      [200, {'Content-Type' => 'text/html'}, ["Hello, I'm #{@name}! </br><h1>#{@image}</h1> </br> #{@new_game} "]]
+      root
+      [200, { 'Content-Type' => 'text/html' }, ["<h1>Hello, I'm #{@name}!</br>
+        #{@image}</h1></br>#{@new_game}"]]
     when '/new_game'
       new_game
-      [200, {'Content-Type' => 'text/html'}, ["#{@treat if @die_message.empty?} </br><h1>#{@image}</h1></br> belly: #{@belly} </br> <progress value=#{@belly} max=30>
-</progress> #{@eat_message} </br> energy: #{@energy} </br> <progress value=#{@energy} max=30>
-</progress> #{@energy_message} </br></br> #{@die_message} </br> #{@new_game unless @die_message.empty?} "]]
-    when "/eat"
+      [200, { 'Content-Type' => 'text/html' }, ["<h1>#{@treat \
+        if @die_message.empty?}
+        #{@die_message}</br>#{@image}</h1>
+        <h3>belly: #{@belly}</br><progress value=#{@belly} max=30>
+        </progress> #{@eat_message}</br></br>
+        energy: #{@energy}</br><progress value=#{@energy} max=30>
+        </progress> #{@energy_message}</h3>
+        #{@new_game unless @die_message.empty?}"]]
+    when '/eat'
       eat
-      [200, {"Content-Type" => "text/html"}, ["#{@treat if @die_message.empty?} </br><h1>#{@image}</h1></br> belly: #{@belly} </br> <progress value=#{@belly} max=30>
-</progress> #{@eat_message} </br> energy: #{@energy} </br> <progress value=#{@energy} max=30>
-</progress> #{@energy_message} </br></br> #{@die_message} </br> #{@new_game unless @die_message.empty?}"]]
-    when "/play"
+      [200, { 'Content-Type' => 'text/html' }, ["<h1>#{@treat \
+        if @die_message.empty?}
+        #{@die_message}</br>#{@image}</h1>
+        <h3>belly: #{@belly}</br><progress value=#{@belly} max=30>
+        </progress> #{@eat_message}</br></br>
+        energy: #{@energy}</br><progress value=#{@energy} max=30>
+        </progress> #{@energy_message}</h3>
+        #{@new_game unless @die_message.empty?}"]]
+    when '/play'
       play
-      [200, {"Content-Type" => "text/html"}, ["#{@treat if @die_message.empty?} </br><h1>#{@image}</h1></br> belly: #{@belly} </br> <progress value=#{@belly} max=30>
-</progress> #{@eat_message} </br> energy: #{@energy} </br> <progress value=#{@energy} max=30>
-</progress> #{@energy_message} </br></br> #{@die_message} </br> #{@new_game unless @die_message.empty?}"]]
-    when "/sleep"
+      [200, { 'Content-Type' => 'text/html' }, ["<h1>#{@treat \
+        if @die_message.empty?}
+        #{@die_message}</br>#{@image}</h1>
+        <h3>belly: #{@belly}</br><progress value=#{@belly} max=30>
+        </progress> #{@eat_message}</br></br>
+        energy: #{@energy}</br><progress value=#{@energy} max=30>
+        </progress> #{@energy_message}</h3>
+        #{@new_game unless @die_message.empty?}"]]
+    when '/sleep'
       sleep
-      [200, {"Content-Type" => "text/html"}, ["#{@treat if @die_message.empty?} </br><h1>#{@image}</h1></br> belly: #{@belly} </br> <progress value=#{@belly} max=30>
-</progress> #{@eat_message} </br> energy: #{@energy} </br> <progress value=#{@energy} max=30>
-</progress> #{@energy_message} </br></br>#{@die_message} </br> #{@new_game unless @die_message.empty?}"]]
+      [200, { 'Content-Type' => 'text/html' }, ["<h1>#{@treat \
+        if @die_message.empty?}
+        #{@die_message}</br>#{@image}</h1>
+        <h3>belly: #{@belly}</br><progress value=#{@belly} max=30>
+        </progress> #{@eat_message}</br></br>
+        energy: #{@energy}</br><progress value=#{@energy} max=30>
+        </progress> #{@energy_message}</h3>
+         #{@new_game unless @die_message.empty?}"]]
     end
   end
 
+  def root
+    feel_well
+  end
+
   def new_game
-    @belly = 15
-    @energy = 15
-    @energy_message = @eat_message = @die_message = ""
-    @image = '&#128516;'
+    feel_well
   end
 
   def eat
     @belly += 6
     @energy -= 3
-    check
+    conditions_check
   end
 
   def play
     @belly -= 3
     @energy -= 3
-    check
+    conditions_check
   end
 
   def sleep
     @belly -= 3
     @energy = 30
-    check
+    conditions_check
   end
 
   private
 
-  def check
-    died if @belly >= 30 || @belly <= 0 || @energy <= 0
-
-    case
-    when @belly > 15 && @belly < 30
-      @eat_message = "#{@name}'s full"
-      @image = '&#128552;'
-    when @belly > 0 && @belly <= 6
-      @eat_message = "#{@name}'s hungry"
-      @image = '&#128543;'
-    when @belly <= 0 || @belly >= 30
-      @eat_message = "it's fatal!"
-      @image = '&#128531;'
-    else
-      @eat_message = nil
-    end
-
-    case
-    when @energy > 0 && @energy <= 8
-      @energy_message = "#{@name}'s tired"
-      @image = '&#128553;'
-    when @energy <= 0
-      @energy_message = "it's fatal!"
-      @image = '&#128531;'
-    else
-      @energy_message = nil
-    end
+  def feel_well
+    @belly = 15
+    @energy = 15
+    @energy_message = @eat_message = @die_message = ''
+    @image = '&#128516;'
   end
 
-  def died
-    @die_message = "Sorry, #{@name} has died".upcase
+  def conditions_check
+    if @belly >= 30 || @belly <= 0 || @energy <= 0
+      @die_message = "Sorry, #{@name} has died".upcase
+    end
+
+    @eat_message = if @belly > 15 && @belly < 30
+                     "&#128552; #{@name}'s full"
+                   elsif @belly > 0 && @belly <= 6
+                     "&#128543; #{@name}'s hungry"
+                   elsif @belly <= 0 || @belly >= 30
+                     "&#128531; it's fatal!"
+                   else
+                     ''
+                   end
+
+    @energy_message = if @energy > 0 && @energy <= 8
+                        "&#128553; #{@name}'s tired"
+                      elsif @energy <= 0
+                        "&#128531; it's fatal!"
+                      else
+                        ''
+                      end
+
+    @image = if @eat_message.empty? && @energy_message.empty?
+               '&#128516;'
+             elsif !@die_message.empty?
+               '&#128128;'
+             else
+               '&#128543;'
+             end
   end
 end
