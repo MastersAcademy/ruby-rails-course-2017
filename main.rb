714a948a7000ae90087fda1c7e4b56f1e89ac74f
kostynskyi <kostynsky@gmail.com>
2018-01-10 23:14:42 +0200

    homework10-11 without Rubocop

--- main.rb
+++ main.rb
@@ -1,91 +1,88 @@
+# This is Employee class
 class Employee
   attr_accessor :first_name
-end
 
-class Waiter < Employee
-
-  def initialize (first_name)
+  def initialize(first_name)
     @first_name = first_name
   end
-  
-  def waiter_comming
-    puts "Customer is comming to the restourant"
-  end
-  
-  def self_presentation
-    puts "Waiter: Hello, My name is #{@first_name}. Are you ready to take an order?"
-  end
-
-  def take_order 
-    puts "Waiter: Please enter the name of the dish you want to order"
-  end
-  
-  def claim_info
-    puts "Waiter: Hey, Your food will be ready in 20 minutes"
-  end
-  
-  def deliver_order
-    puts "Waiter: Here is your order"
-  end
-  
-  def deliver_bill
-    puts "Waiter: Here is your bill"
-  end
-  
-  def thanks
-    puts "Waiter: Thanks for comming and tips!"
-  end
-  
 end
+# This is Waiter class
+class Waiter < Employee
+  def waiter_comming
+    puts 'Customer is comming to the restourant'
+  end
 
+  def self_presentation
+    puts "Waiter: Hello, My name is #{first_name}.
+    Are you ready to take an order?"
+  end
+
+  def take_order
+    puts 'Waiter: Please enter the name of the dish you want to order'
+  end
+
+  def claim_info
+    puts 'Waiter: Hey, Your food will be ready in 20 minutes'
+  end
+
+  def deliver_order
+    puts 'Waiter: Here is your order'
+  end
+
+  def deliver_bill
+    puts 'Waiter: Here is your bill'
+  end
+
+  def thanks
+    puts 'Waiter: Thanks for comming and tips!'
+  end
+end
+ # This is Cook class
  class Cook < Employee
+   def start_coocking
+     puts "Cook: #{first_name}, say to the client,
+      his food will be ready in 20 minutes"
+   end
 
-  def initialize (waiter_name)
+   def food_is_ready
+     puts "Cook: #{first_name}, Take a food and deliver to the customer"
+   end
+ end
+# This is Customer class
+class Customer
+  attr_accessor :waiter_name
+
+  def initialize(waiter_name)
     @waiter_name = waiter_name
   end
-   
-  def start_coocking
-    puts "Cook: #{waiter_name}, say to the client, his food will be ready in 20 minutes"
-  end
 
-  def food_is_ready
-    puts "Cook: #{waiter_name}, Take a food and deliver to the customer"
-  end
-end
-
-class Customer 
-
-  def initialize (waiter)
-    @waiter = waiter
-  end
-  
   def customer_comming
-    puts "Waiter is comming to the customer"
+    puts 'Waiter is comming to the customer'
   end
 
   def new_order
-    @order = gets.chomp
-    puts "Yes, I want to order: #{@order}"
+    order = gets.chomp
+    puts "Yes, I want to order: #{order}"
   end
-  
+
   def waiting_for_food
-    puts "Ok, we are waiting"
+    puts 'Ok, we are waiting'
   end
-    
+
   def ask_for_bill
-    puts "Customer: #{@waiter}, Please give my my bill"
+    puts "Customer: #{waiter_name}, Please give my my bill"
   end
-  
+
   def thanks_you
-    puts "Customer: Thanks for the tasty food! I would like to tip you"
+    puts 'Customer: Thanks for the tasty food! I would like to tip you'
   end
-  
+
   def tips
-    puts "Customer: How much do you want?"
-    @money = gets.chomp
-    puts "Customer: Here are your #{@money} dollars!"
+    puts 'Customer: How much do you want?'
+    money = gets.chomp
+    puts "Customer: Here are your #{money} dollars!"
   end
-end  
+end
 
 waiter = Waiter.new('Antony')
 waiter.waiter_comming
