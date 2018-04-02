8cd118efecac00d786bb333801a6b7462a8be8bd
Vladyslav Volkov <vva_05@ukr.net>
2018-04-02 17:24:16 +0300

    Docker compose

---
+++ ServiceDesk/app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+# Main controller
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  before_action :authenticate_user!
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def initialize_current_user
+    User.current = current_user
+  end
+end
